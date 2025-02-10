098ba4976e6c69ad1bbe3075ecea883db7c125bf
Oliviagiles09 <giles11@usf.edu>
2025-02-09 22:47:06 -0500

    Task 2 - Sales Tax Computation

--- cc_6s.js
+++ cc_6s.js
@@ -11,3 +11,13 @@ calculateProfit(50, 70, 200);
 
 consol.log("Total Profit: $", calculateProfit(20, 30, 100));
 consol.log("Total Profit: $", calculateProfit(50, 70, 200));
+
+// Task 2
+
+let calculateSalesTax = function(amount, taxRate) {
+  let salesTax =Math.floor(amount * taxRate);
+  return salesTax;
+};
+
+consol.log("Sales Tax: $", calculateSalesTax(100, 0.07));
+consol.log("Sales Tax: $", calculateSalesTax(500, 0.1));
